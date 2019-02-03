4c904dda63af8e0c79d03caf42f97a5ee082b24f
ankitshaw <ankitshaw14@gmail.com>
2019-02-03 20:05:00 +0530

    added GET method and return raw html as plain text

--- entity_recogizer.py
+++ entity_recogizer.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
 from flask import Markup, flash
 import spacy
 from spacy import displacy
-from collections import Counter
-from pathlib import Path
 import en_core_web_sm
 
 nlp = en_core_web_sm.load()
@@ -19,6 +17,7 @@ def my_form():
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
+    print(text)
     doc = nlp(text)
     print([(X.text, X.label_) for X in doc.ents])
     svg = displacy.render(doc, style='ent')
@@ -27,7 +26,23 @@ def my_form_post():
 
     return render_template('index.html')
 
+@app.route('/entity', methods=['GET', 'POST'])
+def my_form_get():
+    t = request.args.get('input',default=None, type=str)
+    print(type(t))
+ 
+    text = str(t.encode('utf-8'))
+    doc = nlp(text)
+
+    print([(X.text, X.label_) for X in doc.ents])
+    svg = displacy.render(doc, style='ent', minify=True)
+
+    svg = svg.replace(">b'",">").replace("'</","</")
+    resp =  make_response(svg)
+    resp.mimetype = 'text/plain'
+    return resp
+
 if __name__ == '__main__':
 	import os
 	app.config['SECRET_KEY'] = os.urandom(24).hex()
-	app.run()
+	app.run(host='0.0.0.0',debug=True)
